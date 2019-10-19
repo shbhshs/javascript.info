951b4ed8a7be244936b42adfba4917e92cac0b13
sam081 <shubham08singh10@gmail.com>
2019-10-20 01:03:26 +0530

    created repo and pushed all files

---
+++ 1.script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let value;
+do{
+	value = prompt('input number','0');
+}while(value<=100 &&value);
+
+
+let val = prompt('what is the official name of javascript','');
+if(val == 'ECMAScript')
+	alert('Right');
+else
+	alert('wrong');
+
+/*
+alert("hello");
+[1,2].forEach(alert);
+
+let name = 'John';
+let admin = name;
+alert(`hello ${admin}`);
+*/
